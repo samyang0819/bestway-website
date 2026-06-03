c3db601450f2dd06c460213bfbbef28e36fdc6e5
Samuel Yang <goku67787@gmail.com>
2026-06-03 14:31:05 -0700

    yay

--- components/LiveChatWidget.tsx
+++ components/LiveChatWidget.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { MessageCircle, Send, Phone, X, Clock, CheckCircle } from 'lucide-react'
 
