3bd831b9f713c647c1f6800e3d3b44142f31167f
Samuel Yang <goku67787@gmail.com>
2026-06-03 14:41:33 -0700

    yes

--- components/LiveChatWidget.tsx
+++ components/LiveChatWidget.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
-import { MessageCircle, Send, Phone, X, Clock, CheckCircle } from 'lucide-react'
+import { MessageCircle, Send, X, Clock, CheckCircle } from 'lucide-react'
 
 interface ChatMessage {
   id: string
