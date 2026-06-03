7902658749a33256cb3b21729bbf00ce07a5e8de
Samuel Yang <goku67787@gmail.com>
2026-06-03 16:30:53 -0700

    now

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,19 @@
 'use client'
 
-import Hero from '@/components/Hero'
-import About from '@/components/About'
-import Services from '@/components/Services'
-import { TrustBadges } from '@/components/TrustBadges'
-import { SocialProofCounters } from '@/components/SocialProofCounters'
-import TrustIndicators from '@/components/TrustIndicators'
-import WhyChoose from '@/components/WhyChoose'
-import Statistics from '@/components/Statistics'
-import Testimonials from '@/components/Testimonials'
-import { CaseStudiesCTA } from '@/components/CaseStudiesCTA'
-import FAQ from '@/components/FAQ'
-import Blog from '@/components/Blog'
-import Newsletter from '@/components/Newsletter'
-import Contact from '@/components/Contact'
-import SchemaOrg from '@/components/SchemaOrg'
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 
-// Skip static generation for this page due to component rendering complexity
+// Skip static generation for this page
 export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 export default function Home() {
-  return (
-    <>
-      <SchemaOrg />
-      <Hero />
-      <About />
-      <Services />
-      <TrustBadges />
-      <SocialProofCounters />
-      <TrustIndicators />
-      <WhyChoose />
-      <Statistics />
-      <Testimonials />
-      <CaseStudiesCTA />
-      <FAQ />
-      <Blog />
-      <Newsletter />
-      <Contact />
-    </>
-  )
+  const router = useRouter()
+
+  useEffect(() => {
+    // Redirect to English locale
+    router.push('/en')
+  }, [router])
+
+  return null
 }
