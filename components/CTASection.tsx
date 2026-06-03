0cf0a4d6121bcfcdf4c5eb0bd8d641ea4c703497
Samuel Yang <goku67787@gmail.com>
2026-06-03 14:17:29 -0700

    now

--- components/CTASection.tsx
+++ components/CTASection.tsx
@@ -3,7 +3,7 @@
 import { motion } from 'framer-motion'
 import PremiumButton from './PremiumButton'
 
-interface CTASection Props {
+interface CTASectionProps {
   title: string
   subtitle?: string
   description?: string
