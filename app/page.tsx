09c10a944bee538370e739f4e4f7c82eb782608a
Samuel Yang <goku67787@gmail.com>
2026-06-03 16:35:04 -0700

    now

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { useRouter } from 'next/navigation'
 
 // Skip static generation for this page
 export const dynamic = 'force-dynamic'
-export const revalidate = 0
 
 export default function Home() {
   const router = useRouter()
